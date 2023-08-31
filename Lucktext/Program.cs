e804a99ba625826a13ba5ab04b248a85f9fe0bb0
Wiggo Tibbling <wiggo.tibbling@elev.ga.ntig.se>
2023-08-31 10:26:55 +0200

    första comit

---
+++ Lucktext/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.CompilerServices;
+
+Console.WriteLine("vad du namn?");
+string name = Console.ReadLine();
+Console.WriteLine("var du bor?");
+string hus = Console.ReadLine();
+Console.WriteLine("vad är din bankinformation?");
+string bank = Console.ReadLine();
+Console.WriteLine($"Hej {name} som bor vid {hus}.");
+Console.WriteLine("om detta är din bankinformation tryck Y om det är fel tryck något annat");
+Console.WriteLine($"{bank}");
+string svar = Console.ReadLine();
+if(svar == "Y" || svar == "y"){
+    Console.WriteLine("tack för din hjälp");
+    Console.ReadLine();
+}
+else
+{
+    Console.WriteLine("to bad");
+    Console.ReadLine();
+}
